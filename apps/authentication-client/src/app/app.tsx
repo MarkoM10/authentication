d580cce83b7e3a77f049f33312d6a5d65d2ca79e
MarkoM10 <58051779+MarkoM10@users.noreply.github.com>
2024-02-09 14:29:38 +0100

    Register page, additional input fields, confirm pass logic

--- apps/authentication-client/src/app/app.tsx
+++ apps/authentication-client/src/app/app.tsx
@@ -1,11 +1,11 @@
-import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 import { Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<RegisterPage />} />
       </Routes>
     </div>
   );
--- apps/authentication-client/src/pages/RegisterPage.tsx
+++ apps/authentication-client/src/pages/RegisterPage.tsx
@@ -2,11 +2,21 @@ import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
 
 const LoginPage = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState({
+    pass: false,
+    cfrnmPass: false,
+  });
 
-  const handleTogglePasswordVisibility = () => {
-    setShowPassword((prevState) => !prevState);
+  const handleTogglePasswordVisibility = (type: string) => {
+    console.log(type);
+    if (type === 'pass') {
+      setShowPassword({ ...showPassword, pass: !showPassword.pass });
+    } else {
+      setShowPassword({ ...showPassword, cfrnmPass: !showPassword.cfrnmPass });
+    }
   };
+
+  console.log(showPassword);
 
   const handleOnChange = (e: any) => {
     console.log(e.target.value);
@@ -22,7 +32,7 @@ const LoginPage = () => {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Create your account
           </h2>
         </div>
 
@@ -49,6 +59,26 @@ const LoginPage = () => {
             </div>
 
             <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
+                  required
+                  className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(e) => handleOnChange(e)}
+                />
+              </div>
+            </div>
+
+            <div>
               <div className="flex items-center justify-between">
                 <label
                   htmlFor="password"
@@ -61,17 +91,51 @@ const LoginPage = () => {
                 <input
                   id="password"
                   name="password"
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword.pass ? 'text' : 'password'}
                   autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 px-1.5 py-1.5 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(e) => handleOnChange(e)}
                 />
                 <button
                   type="button"
                   className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-                  onClick={handleTogglePasswordVisibility}
+                  onClick={() => handleTogglePasswordVisibility('pass')}
                 >
-                  {showPassword ? (
+                  {showPassword.pass ? (
+                    <EyeSlashIcon className="w-5 h-5" /> // EyeOffIcon should represent the icon for hiding the password
+                  ) : (
+                    <EyeIcon className="w-5 h-5" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="cnfrmPassword"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm password
+                </label>
+              </div>
+              <div className="mt-2 relative">
+                <input
+                  id="cfrnmpassword"
+                  name="cfrnmpassword"
+                  type={showPassword.cfrnmPass ? 'text' : 'password'}
+                  autoComplete="cfrnmpassword"
+                  required
+                  className="block w-full rounded-md border-0 px-1.5 py-1.5 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(e) => handleOnChange(e)}
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+                  onClick={() => handleTogglePasswordVisibility('cfrnmPass')}
+                >
+                  {showPassword.cfrnmPass ? (
                     <EyeSlashIcon className="w-5 h-5" /> // EyeOffIcon should represent the icon for hiding the password
                   ) : (
                     <EyeIcon className="w-5 h-5" />
